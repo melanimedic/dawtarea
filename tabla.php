8a4cbe3769eea95c097de45acb71a02bccc2e6c7
melanimedic <melanivracaric@gmail.com>
2020-11-05 23:37:47 +0100

    tarea

---
+++ tabla.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="icon" type="icon/png" href="img/icon.png">
+
+	<title>Tabla</title>
+</head>
+<body>
+
+</body>
+</html><style type="text/css">
+.tg  {border-collapse:collapse;border-color:#ccc;border-spacing:0;}
+.tg td{background-color:#fff;border-color:#ccc;border-style:solid;border-width:1px;color:#333;
+  font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:8px 20px;word-break:normal;}
+.tg th{background-color:#f0f0f0;border-color:#ccc;border-style:solid;border-width:1px;color:#333;
+  font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:8px 20px;word-break:normal;}
+.tg .tg-1wig{font-weight:bold;text-align:left;vertical-align:top}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+</style>
+<?php $enumeracion = 1;?>
+<table class="tg">
+<thead>
+  <tr>
+    <th class="tg-1wig">Cardinal</th>
+    <th class="tg-1wig">Ordinal</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Primero</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Segundo</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Tercero</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Cuarto</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Quinto</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Sexto</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Séptimo</td>
+  </tr>
+  <tr>
+   <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Octavo</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Noveno</td>
+  </tr>
+  <tr>
+    <td><?php echo enumeracion; enumeracion++; ?></td>
+    <td class="tg-0lax">Décimo</td>
+  </tr>
+</tbody>
+</table>
